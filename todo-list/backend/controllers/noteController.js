646318e69f7966ccade0e8859340bed8b340116d
Yash Suthar <yashsuthar0309@gmail.com>
2025-01-01 03:36:36 +0530

    error handling #19

--- todo-list/backend/controllers/noteController.js
+++ todo-list/backend/controllers/noteController.js
@@ -1,34 +1,41 @@
+const asyncHandler = require('express-async-handler');
+
 //@desc Get all notes
 //@route GET /api/notes
 //@access public
 
-const getNotes = (req, res) => {
+const getNotes = asyncHandler(async (req, res) => {
     res.status(200).json({messsage: "get Your notes"});
-}
+});
 
 //@desc create new note
 //@route POST /api/notes
 //@access public
 
-const createNote = (req, res) => {
+const createNote = asyncHandler(async (req, res) => {
+    const {text, completed} = req.body;
+    if (!text || !completed) {
+        res.status(400);
+        throw new Error("All fields are mandatory") 
+    }
     res.status(201).json({messsage: "create notes"});
-}
+});
 
 //@desc Update note
 //@route PUT /api/notes/:id
 //@access public
 
-const updateNote = (req, res) => {
+const updateNote = asyncHandler(async (req, res) => {
     res.status(200).json({messsage: `Update notes for ${req.params.id}`});
-}
+});
 
 //@desc delete note
 //@route DELETE /api/notes/:id
 //@access public
 
-const deleteNote = (req, res) => {
+const deleteNote = asyncHandler(async (req, res) => {
     res.status(200).json({messsage: `Delete notes for ${req.params.id}`});
-}
+});
 
 
 module.exports = {getNotes, createNote, updateNote, deleteNote};
--- todo-list/backend/server.js
+++ todo-list/backend/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
 
 const app = express(); 
-app.use(express.json());
-
 const PORT = process.env.PORT || 5000;
 
+
+app.use(express.json());
 app.use("/api/notes", require("./routes/noteRoutes"))
+app.use(errorHandler);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
